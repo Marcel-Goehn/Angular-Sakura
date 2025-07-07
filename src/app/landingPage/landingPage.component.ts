071a62cdaf01ebfe7182edea8979a311ea878775
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-07 20:10:08 +0200

    first commit

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { NavbarComponent } from "./navbar/navbar.component";
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent],
+    template: `
+        <section>
+            <app-navbar></app-navbar>
+            <h1 class="font-raleway">SAKURA RAMEN</h1>
+            <h2 class="font-raleway">THE BEST RAMEN IN TOWN</h2>
+        </section>
+    `,
+    styleUrls: ['./landingPage.component.scss']
+})
+
+export class LandingPageComponent {
+
+}
