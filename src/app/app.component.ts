c05a9a103cba3c7793e9e706cdba129d4d8511de
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-08 17:14:17 +0200

    added imprint, footer, main-content, how-to-order and our-menu components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landingPage/landingPage.component';
+import { MainContentComponent } from "./main-content/main-content.component";
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, CommonModule, MainContentComponent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
