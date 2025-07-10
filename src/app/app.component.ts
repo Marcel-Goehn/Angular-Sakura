45391f3bed19aca75493a4da4851a259e4b56c57
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-10 17:56:45 +0200

    added mixins scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from "./main-content/main-content.component";
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MainContentComponent, FooterComponent, ImprintComponent],
+  imports: [RouterOutlet, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'angular-sakura';
 }
